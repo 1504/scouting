788f322cd6a61901481079a835fd43e234b55f2e
Arham Jain <arhamjain@gmail.com>
2019-02-09 23:21:58 -0500

    changed the public folder

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({
     limit: '100mb'
 }));
 app.use(cors());
-app.use('/', express.static(__dirname + '/'));
+app.use('/', express.static(__dirname + '/dist'));
 fs.writeFile(fileName + ".xlsx", j2xls(data), 'binary', function(err) {
     error(err);
 });
